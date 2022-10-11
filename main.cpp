631293587146e721cd5bb8c29efdd04ff6e30073
Kyuwoong Lee <kwooler@gmail.com>
2022-10-11 00:54:21 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//	Lab 5-11 : Problem Definition
+//	Make a program that find the prime numbers in the given range.
+//		example) find the prime numbers in the range [1..100]
+//		2, 3, 5, 7, ... , 97
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int begin, end;
+
+	do
+	{
+		cout << "Enter your range [from, to] \n";
+		cin >> begin >> end;
+	} while (begin >= end || begin < 2 || end < 0);
+
+	
+	// Complete this code.
+	
+}
